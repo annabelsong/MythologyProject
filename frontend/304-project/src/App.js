d69fee48d3d9bb668bfe4b5d0ae709e981f84fb8
bryanz787 <bryan.yj.zhou@gmail.com>
2024-04-01 18:12:21 -0700

    Renaming tweaks

--- frontend/304-project/src/App.js
+++ frontend/304-project/src/App.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import NaviPage from './NaviPage';
+import SplashPage from './SplashPage';
 import AddPage from './AddPage';
 import ViewPage from './ViewPage';
 import SortPage from './SortPage';
 import SettingsPage from './SettingsPage';
-import { render } from '@testing-library/react'; 
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('NaviPage');
+  const [currentPage, setCurrentPage] = useState('SplashPage');
 
   const renderPage = () => {
     switch (currentPage) {
-      case 'NaviPage':
-        return <NaviPage />;
+      case 'SplashPage':
+        return <SplashPage />;
       case 'AddPage':
         return <AddPage />;
       case 'ViewPage':
@@ -32,13 +31,14 @@ function App() {
     <div className="App">
       <nav className="bg-gray-200 py-5">
         <div className="container mx-auto">
-          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2" onClick={() => setCurrentPage('AddPage')}>Add Entries</button>
-          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2" onClick={() => setCurrentPage('ViewPage')}>View Entries</button>
-          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2" onClick={() => setCurrentPage('SortPage')}>Sort Entries</button>
-          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2" onClick={() => setCurrentPage('SettingsPage')}>Edit Settings</button>
-          <div className="absolute top-20 left-0 bg-black h-1 w-full"></div>
+          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2 font-bold" onClick={() => setCurrentPage('SplashPage')}>Main Menu</button>
+          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2 font-bold" onClick={() => setCurrentPage('AddPage')}>Add Entries</button>
+          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2 font-bold" onClick={() => setCurrentPage('ViewPage')}>View Entries</button>
+          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2 font-bold" onClick={() => setCurrentPage('SortPage')}>Sort Entries</button>
+          <button className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2 font-bold" onClick={() => setCurrentPage('SettingsPage')}>Edit Settings</button>
         </div>
       </nav>
+      <div className="absolute top-20 left-0 bg-black h-1 w-full"></div>
       {renderPage()}
     </div>
   );
---
+++ frontend/304-project/src/SplashPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SplashPage() {
+  return (
+    <div>
+      <h2>I am the splash page</h2>
+    </div>
+  );
+}
+
+export default SplashPage;
