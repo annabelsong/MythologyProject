51fe26a242f9e8054035600e9d6d3b108aa90b33
annabelsong <annaw14@students.cs.ubc.ca>
2024-04-04 21:12:33 -0700

    starting view page

--- frontend/304-project/src/ViewPage.js
+++ frontend/304-project/src/ViewPage.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import LocationView from './viewComponents/LocationView';
+
+
+const tables = [
+  ["Location","location"],
+  ["Ritual","ritual"],
+  ["Tale","tale"],
+  ["Story Events","storyevent"],
+  ["Pantheon","pantheon"],
+  ["Artifact","artifact"],
+  ["Belongs To","belongsto"],
+  ["Appears In","appearsin"],
+  ["Symbol","symbol"],
+  ["Deity","deity"],
+  ["Mortal","mortal"],
+  ["Creature","creature"],
+  ["Represents","represents"],
+  ["Part Of","partof"]
+];
+
 
 function ViewPage() {
+  const [selectedTable, setSelectedTable] = useState('');
+
+  const handleTableChange = (event) => {
+    setSelectedTable(event.target.value);
+  };
+
   return (
-    <div>
-      <h2>I am the view page</h2>
+    <div className="flex flex-col items-start ml-4 mt-4">
+      <h2>Welcome to the View Page, select a table from the drop-down to view its data</h2>
+
+      <div className='h-4'/>{/*spacer*/}
+
+      <select value={selectedTable} onChange={handleTableChange} className="mr-4 bg-white text-blue-500 rounded-full border-2 border-black px-4 py-2 font-bold">
+        <option value="">Select a table</option>
+        {tables.map(([label, value]) => (
+          <option key={value} value={value}>{label}</option>
+        ))}
+      </select>
+
+      <div className='h-4'/>{/*spacer*/}
+
+      {/* Render selected view component */}
+      {selectedTable === 'location' && <LocationView />}
+      {selectedTable === '' && <div>Select A Table First!</div>}
     </div>
   );
 }
---
+++ frontend/304-project/src/viewComponents/LocationView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function LocationView() {
+  return (
+    <div>
+      <h2>I am the locations page</h2>
+    </div>
+  );
+}
+
+export default LocationView;
