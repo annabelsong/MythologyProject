f8800187a542c7ca0869d45c239a6c747c925911
annabelsong <annaw14@students.cs.ubc.ca>
2024-04-05 04:10:34 -0700

    trying to splash?

--- frontend/304-project/src/SplashPage.js
+++ frontend/304-project/src/SplashPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import splashImage from './picture/mythology.png';
+
 
 function SplashPage() {
   return (
     <div>
+      <img src={splashImage} alt="Mythology app splash" />
       <h2>I am the splash page</h2>
     </div>
   );
