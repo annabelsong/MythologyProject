d0a71386da298d014eac26f4995b1fcf479dfab9
UBC Student <klusvi@students.cs.ubc.ca>
2024-04-06 21:30:30 -0700

    location PROJECT functionality

--- backend/server.js
+++ backend/server.js
@@ -989,6 +989,21 @@ app.get('/api/having/CharacterCount', (req, res) => {
 
 
 
+// PROJECT location data in MySQL
+app.get('/api/project/Location', (req, res) => {
+  console.log('Project Location called');
+  const query = "SELECT LocationName, AreaDescription, TimePeriod FROM Location";
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving data from database');
+    } else {
+      const taleNames = results.map(result => result.taleName);
+      res.json(taleNames);
+    }
+  });
+});
+
 
 // Start the server
 app.listen(port, () => {
